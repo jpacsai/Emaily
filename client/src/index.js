09f165266b2f23c88c5b3c0df8981fd8065c6f82
jpacsai <judit.pacsai@gmail.com>
2019-07-11 15:46:48 +0200

    create redux store and reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import store from './store';
 
 import App from './components/App';
-
-const store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
