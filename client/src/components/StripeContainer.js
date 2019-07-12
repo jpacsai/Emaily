f66212a3b8f1a88116be18f644fa7b95d796f9ae
jpacsai <judit.pacsai@gmail.com>
2019-07-12 16:49:21 +0200

    add handleToken thunk

--- client/src/components/StripeContainer.js
+++ client/src/components/StripeContainer.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
+import { handleToken } from './../store/actions';
+
+const mapDispatchToProps = { handleToken };
 
 class StripeContainer extends React.Component {
   render() {
+    const { handleToken } = this.props;
     return (
       <StripeCheckout
         amount={5 * 100}
-        token={token => console.log(token)}
+        token={token => handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
         name="Emaily"
         description="$5 for 5 survey credits"
       >
         <button className="btn">Add credits</button>
       </StripeCheckout>
-    )
+    );
   }
 }
-
-export default StripeContainer;
+export default connect(
+  null,
+  mapDispatchToProps
+)(StripeContainer);
--- client/src/store/actions/thunks.js
+++ client/src/store/actions/thunks.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 import { resolveUser } from './actionCreators';
 
 export const fetchUser = () => async (dispatch) => {
-  const { data } = await axios.get('/api/current_user');
-  dispatch(resolveUser(data));
+  const { userData } = await axios.get('/api/current_user');
+  dispatch(resolveUser(userData));
+}
+
+export const handleToken = (token) => async (dispatch) => {
+  const userData = await axios.post('/api/stripe', token);
+  dispatch(resolveUser(userData));
 }
