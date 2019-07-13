85054affe5f21eb25d235f3c7a8063352edcf3d1
jpacsai <judit.pacsai@gmail.com>
2019-07-13 16:03:12 +0200

    add production routing

--- index.js
+++ index.js
@@ -26,5 +26,14 @@ app.use(passport.session());
 require('./routes/auth')(app);
 require('./routes/billing')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('/client/build'));
+
+  const path = requre('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port);
