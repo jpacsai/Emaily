a0a005d7ff546f3930b6f06e61a2562f4b0edfa4
jpacsai <judit.pacsai@gmail.com>
2019-07-14 15:59:32 +0200

    hotfix

--- index.js
+++ index.js
@@ -29,9 +29,10 @@ require('./routes/billing')(app);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('/client/build'));
 
-  const path = require('path');
+  const root = require('path').join(__dirname, 'client', 'build');
+  app.use(express.static(root));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile('index.html', { root });
   });
 }
 
