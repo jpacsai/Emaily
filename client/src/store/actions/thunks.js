e7a20be5946717efe8312f87278049933b802a03
jpacsai <judit.pacsai@gmail.com>
2019-07-15 10:22:53 +0200

    fix handleToken thunk

--- client/src/store/actions/thunks.js
+++ client/src/store/actions/thunks.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { resolveUser } from './actionCreators';
 
 export const fetchUser = () => async (dispatch) => {
-  const { data } = await axios.get('/api/current_user');
-  dispatch(resolveUser(data));
+  const { data: user } = await axios.get('/api/current_user');
+  dispatch(resolveUser(user));
 }
 
 export const handleToken = (token) => async (dispatch) => {
-  const res = await axios.post('/api/stripe', token);
-  dispatch(resolveUser(res));
+  const { data: user } = await axios.post('/api/stripe', token);
+  dispatch(resolveUser(user));
 }
