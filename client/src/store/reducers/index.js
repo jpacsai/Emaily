44694bbee1a3fd9ba8e683596faace2baac06bb9
jpacsai <judit.pacsai@gmail.com>
2019-07-16 13:30:07 +0200

    add redux-form reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm } from 'redux-form';
 import auth from './auth';
 
 export default combineReducers({
-  auth
+  auth,
+  form: reduxForm
 });
