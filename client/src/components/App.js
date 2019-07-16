f409515e0a86ec11754f9fa4b6b003806e14d07d
jpacsai <judit.pacsai@gmail.com>
2019-07-16 11:59:46 +0200

    add Dashboard component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import { fetchUser } from './../store/actions';
 
 import Header from './Header';
 import Landing from './Landing';
+import Dashboard from './Dashboard';
 
-const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 
 const mapDispatchToProps = { fetchUser };
