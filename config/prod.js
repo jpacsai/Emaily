4529435c194f0631b9218a06723e1fda188a6b61
jpacsai <judit.pacsai@gmail.com>
2019-07-16 11:42:48 +0200

    add REDIRECT_DOMAIN end variable

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,6 @@ module.exports = {
   COOKIE_KEY: process.env.COOKIE_KEY,
   STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
   STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  SEND_GRID_KEY: process.env.SEND_GRID_KEY
+  SEND_GRID_KEY: process.env.SEND_GRID_KEY,
+  REDIRECT_DOMAIN: process.env.REDIRECT_DOMAIN
 }
