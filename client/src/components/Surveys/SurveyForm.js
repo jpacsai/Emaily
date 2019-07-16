f245d95156ab489500d8c077215e79f75fc8b156
jpacsai <judit.pacsai@gmail.com>
2019-07-16 14:21:15 +0200

    improve SurveyForm with SurveyField

--- client/src/components/Surveys/SurveyForm.js
+++ client/src/components/Surveys/SurveyForm.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+  { label: "Survey Title", name: "title"},
+  { label: "Subject Line", name: "subject"},
+  { label: "Email body", name: "body"},
+  { label: "Recipient List", name: "emails"}
+];
 
 class SurveyForm extends React.Component {
+  renderFields() {
+    return (
+      FIELDS.map(({ label, name }, i) => 
+        <Field key={i} type="text" name={name} component={SurveyField} label={label} />
+      )
+    );
+  }
+
   render() {
     return (
       <div>
-        Survey form
+        <form onSubmit={this.props.handleSubmit(res => console.log(res))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }  
 }
 
-export default SurveyForm;
+export default reduxForm({ form: 'surveyForm '})(SurveyForm);
