c115649d62b61060ac8cdcd3a2128214270fdd83
jpacsai <judit.pacsai@gmail.com>
2019-07-17 11:02:08 +0200

    add send button to SurveyFormReview

--- client/src/components/Surveys/SurveyFormReview.js
+++ client/src/components/Surveys/SurveyFormReview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { formFields } from './../../config';
+import { submitSurvey } from './../../store/actions/thunks';
 
 const mapStateToProps = state => ({
   formValues: state.form.surveyForm.values
@@ -10,6 +11,7 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
   return (
     <div>
       <h5>Please check your details</h5>
+
       { formFields.map(({ label, name}) => (
         <div key={name}>
           <label>{label}</label>
@@ -17,8 +19,13 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
         </div>
       )) }
 
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+      <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
         Back
+      </button>
+
+      <button className="green white-text btn-flat right" onClick={() => submitSurvey(formValues)}>
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
--- client/src/store/actions/thunks.js
+++ client/src/store/actions/thunks.js
@@ -11,3 +11,7 @@ export const handleToken = (token) => async (dispatch) => {
   const { data: user } = await axios.post(paths.STRIPE, token);
   dispatch(resolveUser(user));
 }
+
+export const submitSurvey = (formValues) => {
+  console.log(formValues);
+}
