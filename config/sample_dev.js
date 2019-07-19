68084852c16d5f8399641c6fce76f82280f95343
jpacsai <judit.pacsai@gmail.com>
2019-07-19 13:23:23 +0200

    add sample_dev.js

---
+++ config/sample_dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  GOOGLE_CLIENT_ID: '',
+  GOOGLE_CLIENT_SECRET: '',
+  MONGO_URI: '',
+  COOKIE_KEY: '',
+  STRIPE_PUBLISHABLE_KEY: '',
+  STRIPE_SECRET_KEY: '',
+  SEND_GRID_KEY: '',
+  REDIRECT_DOMAIN: 'http://localhost:3000'
+}
