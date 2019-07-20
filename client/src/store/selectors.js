d23da4d98385d5b55e0785eaa996a12d0aa1b9e1
jpacsai <judit.pacsai@gmail.com>
2019-07-20 15:43:39 +0200

    add selectors

---
+++ client/src/store/selectors.js
@@ -0,0 +1,12 @@
+import { createSelector } from 'reselect';
+
+export const getMe = state => state.auth;
+
+export const getSurveys = state => state.surveys;
+
+export const getForm = state => state.form;
+
+export const getFormValues = state => createSelector(
+  getForm,
+  form => (form && form.surveyForm && form.surveyForm.values) || null
+);
