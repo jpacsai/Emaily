e0310664ff8e5f2f35707bc1b9d049296de43909
jpacsai <judit.pacsai@gmail.com>
2019-07-25 09:41:10 +0200

    add delete user route

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const { ObjectId } = require('mongodb');
 
 module.exports = app => {
   app.get(
@@ -23,5 +26,16 @@ module.exports = app => {
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
+  });
+
+  app.delete('/api/current_user', async (req, res) => {
+    if (!req.user) return;
+
+    const id = new ObjectId(req.user.id);
+
+    const { deletedCount } = await User.deleteOne({ _id: id }).exec();
+    if (!!deletedCount) console.log(`Deleted user: ${id}`);
+
+    res.status(!!deletedCount ? 200 : 404).send();
   });
 };
