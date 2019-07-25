b98b44e631e1492645949cb1fabf1421168b0e4f
jpacsai <judit.pacsai@gmail.com>
2019-07-25 13:00:11 +0200

    add DeleteSurveyPrompt

---
+++ client/src/components/Surveys/DeleteSurveyPrompt.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Prompt from '../Prompt';
+
+import './DeleteSurveyPrompt.scss';
+
+const DeleteSurveyPrompt = ({ isOpen, onSubmit, onCancel, surveyTitle }) => {
+  return (
+    <Prompt
+      className="DeleteSurveyPrompt"
+      isOpen={isOpen}
+      onSubmit={onSubmit}
+      submitLabel="Yes, delete!"
+      onCancel={onCancel}
+      title="Delete survey"
+    >
+      <p>Are you sure you want to delete "<span>{surveyTitle}</span>" survey?</p>
+    </Prompt>
+  );
+};
+
+export default DeleteSurveyPrompt;
--- client/src/components/Surveys/SurveyList.js
+++ client/src/components/Surveys/SurveyList.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { getSurveys } from './../../store/selectors';
 import { fetchSurveys, deleteSurvey } from '../../store/actions';
 
+import DeleteSurveyPrompt from './DeleteSurveyPrompt';
+
 const mapStateToProps = state => ({
   surveys: getSurveys(state)
 });
@@ -10,12 +12,32 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = { fetchSurveys, deleteSurvey };
 
 class SurveyList extends React.Component {
+  state = {
+    isPromptOpen: false,
+    deleteSurveyId: null
+  };
+
   componentDidMount() {
     this.props.fetchSurveys();
   }
 
-  handleClick = surveyId => {
-    this.props.deleteSurvey(surveyId);
+  openPrompt = surveyId => {
+    this.setState(state => ({ isPromptOpen: !state.isPromptOpen, deleteSurveyId: surveyId }));
+  };
+
+  closePrompt = () => {
+    this.setState({ isPromptOpen: false, deleteSurveyId: null });
+  };
+
+  handleDelete = () => {
+    const { deleteSurveyId } = this.state;
+    this.closePrompt();
+    this.props.deleteSurvey(deleteSurveyId);
+  };
+
+  getSurveyTitle = () => {
+    const survey = this.props.surveys.find(survey => survey._id === this.state.deleteSurveyId);
+    return survey ? survey.title : '';
   };
 
   renderSurveys() {
@@ -23,15 +45,17 @@ class SurveyList extends React.Component {
     return surveys.map((survey, i) => (
       <div key={i} className="card red lighten-5">
         <div className="card-action right">
-          <button className="btn-flat red white-text" onClick={() => this.handleClick(survey._id)}>
+          <button className="btn-flat red white-text" onClick={() => this.openPrompt(survey._id)}>
             Delete<i className="material-icons right">delete</i>
           </button>
         </div>
+
         <div className="card-content">
           <span className="card-title">{survey.title}</span>
           <p>{survey.body}</p>
           <p className="right">Sent on: {new Date(survey.date_sent).toLocaleDateString()}</p>
         </div>
+        
         <div className="card-action">
           <p>Yes: {survey.yes}</p>
           <p>No: {survey.no}</p>
@@ -41,7 +65,17 @@ class SurveyList extends React.Component {
   }
 
   render() {
-    return <div>{this.renderSurveys()}</div>;
+    return (
+      <div>
+        {this.renderSurveys()}
+        <DeleteSurveyPrompt
+          isOpen={this.state.isPromptOpen}
+          onSubmit={this.handleDelete}
+          onCancel={this.closePrompt}
+          surveyTitle={this.getSurveyTitle()}
+        />
+      </div>
+    );
   }
 }
 
