c5f967e421f9a86d7c0a41a43e4e833cee702134
jpacsai <judit.pacsai@gmail.com>
2019-08-05 14:27:33 +0200

    add parameterized selector getSurveyResults

--- client/src/store/selectors.js
+++ client/src/store/selectors.js
@@ -1,7 +1,28 @@
-// import { createSelector } from 'reselect';
+import { createSelector } from 'reselect';
 
 export const getMe = state => state.auth;
 
 export const getSurveys = state => state.surveys;
+
+export const getSurvey = createSelector(
+  [getSurveys, (state, id) => id],
+  (surveys, id) => {
+    if (!surveys) return null;
+    const survey = surveys.find(survey => survey._id === id);
+    return survey || null;
+  }
+);
+
+export const getSurveyResults = createSelector(
+  getSurvey,
+  survey => {
+    if (!survey) return null;
+    const { yes, no } = survey;
+    return {
+      yes,
+      no
+    }
+  }
+)
 
 export const getForm = state => state.form;
