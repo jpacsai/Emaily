413f539502126440b4342c039ce93c00524bcc71
jpacsai <judit.pacsai@gmail.com>
2019-08-09 15:34:27 +0200

    improve SortBy

--- client/src/components/common/SortBy.js
+++ client/src/components/common/SortBy.js
@@ -3,11 +3,16 @@ import Select from 'react-select';
 
 import './SortBy.scss';
 
-const SortBy = ({ options }) => {
+const SortBy = ({ options, defaultValue, onChange }) => {
+  const defaultOption = options.filter(option => option.value === defaultValue);
+
+  const handleChange = ({value}) => {
+    onChange(value);
+  }
   return (
     <div className="SortBy">
       <p>Sort by: </p>
-      <Select options={options} />
+      <Select options={options} defaultValue={defaultOption} onChange={(option) => handleChange(option)}/>
     </div>
   );
 };
