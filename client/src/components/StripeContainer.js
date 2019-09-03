1536ff264aff671dd9e393f62374d14cafc2dc08
jpacsai <judit.pacsai@gmail.com>
2019-09-04 00:30:44 +0200

    Style StripeContainer button

--- client/src/components/StripeContainer.js
+++ client/src/components/StripeContainer.js
@@ -3,21 +3,26 @@ import { connect } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
 import { handleToken } from './../store/actions';
 
+import './StripeContainer.scss';
+
 const mapDispatchToProps = { handleToken };
 
 class StripeContainer extends React.Component {
   render() {
     const { handleToken } = this.props;
     return (
-      <StripeCheckout
-        amount={5 * 100}
-        token={token => handleToken(token)}
-        stripeKey={process.env.REACT_APP_STRIPE_KEY}
-        name="Emaily"
-        description="$5 for 5 survey credits"
-      >
-        <button className="btn">Add credits</button>
-      </StripeCheckout>
+      <div className="StripeCheckout">
+        <StripeCheckout
+          amount={5 * 100}
+          token={token => handleToken(token)}
+          stripeKey={process.env.REACT_APP_STRIPE_KEY}
+          name="Emaily"
+          description="$5 for 5 survey credits"
+          ComponentClass="div"
+        >
+          <button className="btn">Add credits</button>
+        </StripeCheckout>
+      </div>
     );
   }
 }
